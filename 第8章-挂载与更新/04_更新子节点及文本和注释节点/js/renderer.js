cbdbdb45902924ff9d926c4363e5022afea41e43
Baoguoq1ang <18701094030@163.com>
2022-04-22 09:18:34 +0800

    更新子节点及文本和注释节点的处理

---
+++ 第8章-挂载与更新/04_更新子节点及文本和注释节点/js/renderer.js
@@ -0,0 +1,148 @@
+// 渲染器函数的实现
+function createRenderer(options) {
+    const {
+        createElement,
+        setElementText,
+        insert,
+        patchProps,
+        createText,
+        setText,
+        createComment,
+        setComment
+    } = options;
+    const Text = Symbol();
+    const Comment = Symbol();
+
+    function mountElement(vnode, container) {
+        const el = vnode.el = createElement(vnode.type);
+
+        if (typeof vnode.children === 'string') {
+            setElementText(el, vnode.children);
+        } else if (Array.isArray(vnode.children)) {
+            vnode.children.forEach(child => {
+                patch(null, child, el);
+            });
+
+        }
+
+        if (vnode.props) {
+            for (const key in vnode.props) {
+                patchProps(el, key, null, vnode.props[key])
+            }
+        }
+        insert(el, container);
+    }
+
+    function patch(n1, n2, container) {
+        // 如果新旧 vnode 的类型不同，则直接将旧 vnode 卸载
+        if (n1 && n1.type !== n2.type) {
+            unmount(n1);
+            n1 = null;
+        }
+        const { type } = n2;
+        if (typeof type === 'string') {
+            if (!n1) {
+                // 如果 n1 不存在表示意味着挂载，调用 mountElement 函数完成挂载
+                mountElement(n2, container);
+            } else {
+                // n1 存在，意味着打补丁
+                patchElement(n1, n2);
+            }
+        } else if (typeof type === 'object') {
+            // 如果 n2 的 type 是个对象，那就说明是个组件
+        } else if (typeof type === Text) {
+            // 文本节点
+            if (!n1) {
+                const el = n2.el = createText(n2.children);
+                insert(el, container);
+            } else {
+                const el = n2.el = n1.el;
+                if (n2.children !== n1.children) {
+                    setText(el, n2.children);
+                }
+            }
+        } else if (typeof type === Comment) {
+            // 注释节点
+            if (!n1) {
+                const el = n2.el = createComment(n2.children);
+                insert(el, container);
+            } else {
+                const el = n2.el = n1.el;
+                if (n2.children !== n1.children) {
+                    setComment(el, n2.children);
+                }
+            }
+        }
+    }
+
+    function render(vnode, container) {
+        console.log(vnode);
+        if (vnode) {
+            patch(container._vnode, vnode, container);
+        } else {
+            if (container._vnode) {
+                // document.body.innerHTML = '';
+                unmount(container._vnode);
+                
+            }
+        }
+        container._vnode = vnode;
+    }
+
+    function unmount(vnode) {
+        const parent = vnode.el.parentNode;
+        if (parent) parent.removeChild(vnode.el);
+    }
+
+    // ————————————更新子节点 开始————————————
+    
+    function patchElement(n1, n2) {
+        const el = n2.el = n1.el;
+        const oldProps = n1.props;
+        const newProps = n2.props;
+        for (const key in newProps) {
+            if (newProps[key] !== oldProps[key]) {
+                patchProps(el, key, oldProps[key], newProps[key]);
+            }
+        }
+        for (const key in oldProps) {
+            if (!(key in newProps)) {
+                patchProps(el, key, oldProps[key], null);
+            }
+        }
+        patchChildren(n1, n2, el);
+    }
+
+    function patchChildren(n1, n2, container) {
+        // 判断新子节点是一个文本节点
+        if (typeof n2.children === 'string') {
+            if (Array.isArray(n1.children)) {
+                n1.children.forEach(c => unmount(c));
+            }
+            setElementText(container, n2.children);
+        } else if (Array.isArray(n2.children)) {
+            if (Array.isArray(n1.children)) {
+                // diff算法, 因还未到 diff 算法章节，暂时用傻瓜式方法，旧节点全部卸载后挂载新节点
+                n1.children.forEach(c => unmount(c));
+                n2.children.forEach(c => patch(null, c, container));
+            } else {
+                setElementText(container, '');
+                n2.children.forEach(c => patch(null, c, container));
+            }
+            
+        } else {
+            // 代码运行到这里，说明新子节点不存在
+            if (Array.isArray(n1.children)) {
+                n1.children.forEach(c => unmount(c));
+            } else {
+                setElementText(container, '');
+            }
+        }
+    }
+
+    // ————————————更新子节点 结束————————————
+
+    return {
+        render
+    }
+}
